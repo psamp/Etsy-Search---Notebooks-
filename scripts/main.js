e702adf05ce549a3bec99d59d4fac3ad91d50a5d
Princess Sampson <ps@kingonomics.com>
2015-05-24 09:51:03 -0400

    styled sidebar links, styled items, still need to organize item data

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,45 @@
-console.log('This is cool!');
+$(document).ready( function() {
+
+// GENERATE SIDEBAR CATOGORIES
+
+var sidebar = items.results.map ( function (item) { return item.taxonomy_path;});
+
+var sidebarItems = [];
+sidebar.forEach(function (list) {
+    list.forEach(function (item){
+     if (sidebarItems.indexOf(item) === -1){
+    sidebarItems.push(item);
+    }
+  });
+});
+
+
+var sidebarUl = $("#sidebarCats");
+sidebarItems.forEach( function (sbItem) {
+  sidebarUl.append("<li><a href='#'>" + sbItem + "</a></li>");
+});
+
+
+// // ITEMS
+
+// var itemCont = $(".items");
+// items.results.forEach( function (item) {
+
+//   itemCont.append("<div class='item'><p class='itemtitle'>" + item.title + "</p></div>")};
+
+
+var $itemData = $(".items");
+var itemImage, itemPrice, itemTitle, itemSeller, itemHolder;
+
+items.results.forEach( function(etsy) {
+  
+  itemPrice = "<p class='price'>" + "$" + etsy.price + " USD" +"</p>";
+  itemImage ="<img src='" + etsy.Images[0].url_170x135 +"''>";
+  itemTitle = "<p class ='title'>" + etsy.title + "</p>";
+  itemSeller = "<p class='seller'>" + etsy.Shop.shop_name + "</p>";
+  itemHolder = "<li class='itemData'>" + itemImage + itemTitle + itemSeller + itemPrice + "</li>";
+  
+  $itemData.append(itemHolder);
+  });
+
+});
